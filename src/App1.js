274f13403c7e04e234b71490406dc9ca4ce0f875
Marc <marc.bieber@epitech.eu>
2023-11-21 14:55:27 -0500

    ui

--- src/App1.js
+++ src/App1.js
@@ -1,12 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const WeatherApp = () => {
+  const [location, setLocation] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+
+  const apiKey = '5c6c50a2128640b6bfb194857232111';
+
+  const fetchWeather = async () => {
+    try {
+      const response = await axios.get(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=chicoutimi&lang=fr`);
+      setWeatherData(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error("Erreur lors de la récupération des données météo", error);
+    }
+  };
+
+  React.useEffect(() => {
+    fetchWeather();
+  }, []);
+
   return (
-    <div className="quarter-grid-container">
-      <div className="grid-item">1</div>
+    <div>
+      {weatherData && (
+        <div>
+          <h3>Météo à {weatherData.location.name}</h3>
+          <p>Température: {weatherData.current.temp_c} °C</p>
+          <p>Condition: {weatherData.current.condition.text}</p>
+        </div>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default WeatherApp;
