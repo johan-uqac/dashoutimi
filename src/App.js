4c80c414712de7cfb04f990a4f1a7d628fa7c781
Johan Chrillesen <johan.chrillesen@epitech.eu>
2023-11-21 14:33:18 -0500

    grid

--- src/App.js
+++ src/App.js
@@ -3,21 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="quarter-grid-container">
+      <div className="grid-item">1</div>
+      <div className="grid-item">2</div>
+      <div className="grid-item">3</div>
+      <div className="grid-item">4</div>
     </div>
   );
 }
