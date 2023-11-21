3ddd5f50f3c94f0106201e6e0357584e02302479
matt-tek <mathieu.muty@epitech.eu>
2023-11-21 16:37:56 -0500

    [FEAT] display event list

--- src/App3.js
+++ src/App3.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './data.json'
 
 function App() {
+  const listItem = data.map(info => {
+      return (
+        <li>
+          <p>{info.date}</p>
+          <p>{info.description}</p>
+          <p>{info.hour}</p>
+        </li>
+      )
+  })
+
   return (
     <div className="quarter-grid-container">
       <div className="grid-item">3</div>
+      <div>{listItem}</div>
     </div>
   );
 }
