dd990ef8ed582fb6ab3764b1d67462a836aa36cd
Johan Chrillesen <johan.chrillesen@epitech.eu>
2023-11-21 19:23:52 -0500

    Add population stats file

---
+++ src/population_stats.js
@@ -0,0 +1,23 @@
+const populationStats = {
+    totalPopulation: 104230,
+    menPopulation: 50995,
+    womenPopulation: 53235,
+    agePopulation: [
+        {
+            age: '0-14',
+            count: 14625,
+        },
+        {
+            age: '15-64',
+            count: 66525,
+        },
+        {
+            age: '65+',
+            count: 23080,
+        },
+    ],
+    density: 1153.8,
+    area: 90.33,
+}
+
+export { populationStats };
