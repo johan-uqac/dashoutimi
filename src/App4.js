b24d5aaf1723371c484dc919ebd696fc236a6d12
Johan Chrillesen <johan.chrillesen@epitech.eu>
2023-11-21 19:22:56 -0500

    Add population stats

--- src/App4.js
+++ src/App4.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
-import './App.css';
+import './App4.css';
+import { populationStats } from './population_stats.js';
 
 function App4() {
   return (
-    <div>
-      <div>4</div>
+    <div className="population-info-container">
+      <h2>Population: {populationStats.totalPopulation} personnes</h2>
+      <h2>Population par age:</h2>
+      <ul>
+        {populationStats.agePopulation.map(ageGroup => (
+          <li key={ageGroup.age}>
+            {ageGroup.age}: {ageGroup.count} habitants
+          </li>
+        ))}
+      </ul>
+      <h2>Distribution par sexe:</h2>
+      <p>Male: {populationStats.menPopulation} ({(populationStats.menPopulation * 100 / populationStats.totalPopulation).toPrecision(2)}%)</p>
+      <p>Female: {populationStats.womenPopulation} ({(populationStats.womenPopulation * 100 / populationStats.totalPopulation).toPrecision(2)}%)</p>
+      <h2>Area: {populationStats.area} kilomètres carrés</h2>
+      <h2>Density: {populationStats.density} habitants par kilomètre carré</h2>
     </div>
   );
 }
